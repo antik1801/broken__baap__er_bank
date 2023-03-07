da45b31f04cf11c0a231da21247e45c9a589e83c
antik1801 <antik.edu@gmail.com>
2023-03-07 22:33:32 +0600

    Diposit total added

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,4 @@
+// This function will get the input value from text-field
 function getInputFieldValueById(inputFieldId) {
   const inputField = document.getElementById(inputFieldId);
   const inputFieldValueString = inputField.value;
@@ -5,15 +6,15 @@ function getInputFieldValueById(inputFieldId) {
   return inputFieldValue;
   inputField.value = "";
 }
-
+// This function will set the element values in innerText
 function getTextElementValueById(elementId) {
   const textElement = document.getElementById(elementId);
   const textElementValueString = textElement.innerText;
-  const textElementValue = textElementValueString;
-  textElementValue;
+  const textElementValue = parseFloat(textElementValueString);
+  return textElementValue;
 }
-
+// This function will set the input text area 
 function setTextElementValueById(elementId, newValue) {
   const textElement = document.getElementById(elementId);
-  textElement.value = newValue;
+  textElement.innerText = newValue;
 }
